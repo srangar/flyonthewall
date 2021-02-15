3ed7c6be0a81259224637062279a2f1806038f55
Srinivasan Rangarajan <srangarajan@riverbed.com>
2021-02-14 22:12:49 -0800

    Get/Create Customer Lambdas Added

--- fly_on_the_wall/lambda_handlers/customer_handlers.py
+++ fly_on_the_wall/lambda_handlers/customer_handlers.py
@@ -1,6 +1,23 @@
+import json
+import uuid
+from fly_on_the_wall.customer import Customer
+
+
 def create_customer(event, _):
-    pass
+    alexa_notif_bearer = event["alexa_bearer_token"]
+    customer_id = str(uuid.uuid4())
+    customer = Custamer(customer_id=customer_id, alexa_notif_bearer=alexa_notif_bearer)
+    customer.save(create=True)
+
+    return {"statusCode": 201}
 
 
 def get_customer(event, _):
-    pass
+    try:
+        customer_id = event["customer_id"]
+        customer = Customer.load(customer_id=customer_id)
+
+        return {"statusCode": 200, "body": json.dumps(customer.to_json())}
+    except exceptions.UserLoadError as err:
+        print(f"Unable to Load Customer: {customer_id}")
+        return {"statusCode": 404}
